8e6644f9ecacde1e80ddd3561e09da7dd632a4a7
Kaila Govardhini K <141433548+kailagovardhinik@users.noreply.github.com>
2023-11-01 15:22:12 +0530

    Update data_warehouse.py

--- data_warehouse.py
+++ data_warehouse.py
@@ -4,15 +4,28 @@ import pandas as pd
 import numpy as np
 import json
 import re
+#creating a function for connection
+def Api_connect():
+    api_key="AIzaSyDj5WwmFp4Qu03Px3qKzU0o0KsdrV_IJsw"
+    api_service_name="youtube"
+    api_version="v3"
+    youtube = build(api_service_name,api_version, developerKey=api_key)
+    return youtube
+youtube=Api_connect()
 
-api_key="AIzaSyDj5WwmFp4Qu03Px3qKzU0o0KsdrV_IJsw"
-youtube = build('youtube', 'v3', developerKey=api_key)
-channel_id="UCxwitsUVNzwS5XBSC5UQV8Q"
-# selected **channel** from resource menu
-
-response = youtube.channels().list(
-    id=channel_id,
-    part='snippet,statistics,contentDetails'
-)
-
-channel_data = response.execute()
+#creating a function to get channel information
+def channel_information(input_id):
+    request = youtube.channels().list(
+        part="snippet,statistics,contentDetails",
+        id=input_id
+    )
+    response = request.execute()
+    for i in response['items']:
+        data = dict(Channel_Name=i["snippet"]["title"],
+                    Channel_id=i["id"],
+                    Subscriber=i["statistics"]["subscriberCount"],
+                    Views=i["statistics"]['viewCount'],
+                    Total_Videos=i["statistics"]['videoCount'],
+                    Channel_Description=i["snippet"]['description'],
+                    Playlist_id=i['contentDetails']['uploads'])
+    return data
