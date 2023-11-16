44226b890a0fcbf96c0b196ec810eab540d6583d
Kaila Govardhini K <141433548+kailagovardhinik@users.noreply.github.com>
2023-11-16 09:09:13 +0530

    data_warehouse.py

--- data_warehouse.py
+++ data_warehouse.py
@@ -527,14 +527,14 @@ question = st.selectbox(
 
 if question == '1. All the videos and the Channel Name':
     query1 = "select Title as videos, Channel_Name as ChannelName from videos;"
-    cursor.execute(query1)
+    mycursor.execute(query1)
     mydb.commit()
     t1=cursor.fetchall()
     st.write(pd.DataFrame(t1, columns=["Video Title","Channel Name"]))
 
 elif question == '2. Channels with most number of videos':
     query2 = "select Channel_Name as ChannelName,Total_Videos as NO_Videos from channels order by Total_Videos desc;"
-    cursor.execute(query2)
+    mycursor.execute(query2)
     mydb.commit()
     t2=cursor.fetchall()
     st.write(pd.DataFrame(t2, columns=["Channel Name","No Of Videos"]))
@@ -542,14 +542,14 @@ elif question == '2. Channels with most number of videos':
 elif question == '3. 10 most viewed videos':
     query3 = '''select Views as views , Channel_Name as ChannelName,Title as VideoTitle from videos 
                         where Views is not null order by Views desc limit 10;'''
-    cursor.execute(query3)
+    mycursor.execute(query3)
     mydb.commit()
     t3 = cursor.fetchall()
     st.write(pd.DataFrame(t3, columns = ["views","channel Name","video title"]))
 
 elif question == '4. Comments in each video':
     query4 = "select Comments as No_comments ,Title as VideoTitle from videos where Comments is not null;"
-    cursor.execute(query4)
+    mycursor.execute(query4)
     mydb.commit()
     t4=cursor.fetchall()
     st.write(pd.DataFrame(t4, columns=["No Of Comments", "Video Title"]))
@@ -557,21 +557,21 @@ elif question == '4. Comments in each video':
 elif question == '5. Videos with highest likes':
     query5 = '''select Title as VideoTitle, Channel_Name as ChannelName, Likes as LikesCount from videos 
     where Likes is not null order by Likes desc;'''
-    cursor.execute(query5)
+    mycursor.execute(query5)
     mydb.commit()
     t5 = cursor.fetchall()
     st.write(pd.DataFrame(t5, columns=["video Title","channel Name","like count"]))
 
 elif question == '6. likes of all videos':
     query6 = '''select Likes as likeCount,Title as VideoTitle from videos;'''
-    cursor.execute(query6)
+    mycursor.execute(query6)
     mydb.commit()
     t6 = cursor.fetchall()
     st.write(pd.DataFrame(t6, columns=["like count","video title"]))
 
 elif question == '7. views of each channel':
     query7 = "select Channel_Name as ChannelName, Views as Channelviews from channels;"
-    cursor.execute(query7)
+    mycursor.execute(query7)
     mydb.commit()
     t7=cursor.fetchall()
     st.write(pd.DataFrame(t7, columns=["channel name","total views"]))
@@ -579,14 +579,14 @@ elif question == '7. views of each channel':
 elif question == '8. videos published in the year 2022':
     query8 = '''select Title as Video_Title, Published_Date as VideoRelease, Channel_Name as ChannelName from videos 
                 where extract(year from Published_Date) = 2022;'''
-    cursor.execute(query8)
+    mycursor.execute(query8)
     mydb.commit()
     t8=cursor.fetchall()
     st.write(pd.DataFrame(t8,columns=["Name", "Video Publised On", "ChannelName"]))
 
 elif question == '9. average duration of all videos in each channel':
     query9 =  "SELECT Channel_Name as ChannelName, AVG(Duration) AS average_duration FROM videos GROUP BY Channel_Name;"
-    cursor.execute(query9)
+    mycursor.execute(query9)
     mydb.commit()
     t9=cursor.fetchall()
     t9 = pd.DataFrame(t9, columns=['ChannelTitle', 'Average Duration'])
@@ -601,7 +601,7 @@ elif question == '9. average duration of all videos in each channel':
 elif question == '10. videos with highest number of comments':
     query10 = '''select Title as VideoTitle, Channel_Name as ChannelName, Comments as Comments from videos 
     where Comments is not null order by Comments desc;'''
-    cursor.execute(query10)
+    mycursor.execute(query10)
     mydb.commit()
     t10=cursor.fetchall()
     st.write(pd.DataFrame(t10, columns=['Video Title', 'Channel Name', 'NO Of Comments']))
